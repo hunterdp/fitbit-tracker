00f9e8456d1556daed31b852db0fce7a75e09d4a
David Hunter <dph@alumni.neu.edu>
2019-10-08 15:18:17 -0700

    Fixed bug when there was more than 1 sleep event in a day.

--- fitbit-tracker.py
+++ fitbit-tracker.py
@@ -1,4 +1,4 @@
-#/usr/bin/python3
+# /usr/bin/python3
 # -*- coding: utf-8 -*-
 # MIT License
 
@@ -62,121 +62,11 @@ __AUTHOR__ = 'David Hunter'
 __VERSION__ = 'fitbit-tracker ver 1-0'
 __LOG_NAME__ = 'fitbit-tracker.log'
 __TITLE__ = 'fitebit-tracker.py'
+__DEBUG__ = False
 
 # Set a global for the name of the configuration file.  This is used during the Oauth2 callback
 # routine when we need to refresh the tokens.
 CONFIG_FILE = ''
-
-def refresh_new_token(token):
-    """Called when the access token needs to be refreshed. """
-    new_access_token = token['access_token']
-    new_refresh_token = token['refresh_token']
-    new_expires_at = token['expires_at']
-    logging.info('Refreshing token.')
-    logging.info('New token will expire at: ' + str(new_expires_at))
-
-    # open the configuration file and save the new tokens.
-    with open(CONFIG_FILE) as json_config_file:
-        data = json.load(json_config_file)
-
-    data['access_token'] = new_access_token
-    data['refresh_token'] = new_refresh_token
-    data['token_expires'] = new_expires_at
-    with open(CONFIG_FILE, 'w') as j_config_file:
-        json.dump(data, j_config_file, indent=4)
-
-def is_valid_date(date_to_check):
-  """ Checks to see if the date is valid """
-  year,month,day = date_to_check.split('-',3)
-  try :
-    date(int(year), int(month), int(day))
-  except ValueError :
-    return(False)
-  return(True)
-
-def get_heartrate(oauth_client, start_date, time_interval, results_file):
-  """ Retrieve the intraday heartrate data and store to a file.
-
-  Args:
-    oauth_client:  An OAuth2 client id.
-    start_date:    Collect starting at this date
-    end_date:      Stope collecting at this date
-    time_interval: Time ganualarity to collect. See fitbit documentation
-    results_file:  The name of the file to store results in
-
-  Returns:
-    A pandas dataframe with the time and values.
-  """
-  hr = oauth_client.intraday_time_series(resource='activities/heart',
-                                           base_date=start_date,
-                                           detail_level=time_interval,
-                                           start_time='00:00',
-                                           end_time='23:59')
-  logging.debug(json.dumps(hr, indent=2))
-  # Only save the data if there is any.
-  if hr['activities-heart'][0]['value'] != 0:
-      t_list = []
-      v_list = []
-      for i in hr['activities-heart-intraday']['dataset']:
-          v_list.append(i['value'])
-          t_list.append(i['time'])
-  else:
-      logging.info("No heartrate data for " + str(start_date))
-
-  df = pandas.DataFrame({'Time': t_list, start_date: v_list})
-  df.to_csv(results_file, columns=[
-              'Time', start_date], header=True, index=False)
-  return(df)
-
-
-def get_steps(oauth_client, start_date, time_interval, results_file):
-    """Retrieve the step count for the day at the specified interval, store
-       data in a file and returns the data in a panda dataframe.
-    """
-    steps = oauth_client.intraday_time_series(resource='activities/steps',
-                                              base_date=start_date,
-                                              start_time='00:00',
-                                              end_time='23:59',
-                                              detail_level=time_interval)
-    logging.debug(json.dumps(steps, indent=2))
-
-    # Only save the data if there is any.
-    if steps['activities-steps'][0]['value'] != 0:
-        t_list = []
-        v_list = []
-        for i in steps['activities-steps-intraday']['dataset']:
-            v_list.append(i['value'])
-            t_list.append(i['time'])
-    else:
-        logging.info("No step data for " + str(start_date))
-
-    df = pandas.DataFrame({'Time': t_list, start_date: v_list})
-    df.to_csv(results_file, columns=[
-              'Time', start_date], header=True, index=False)
-    return(df)
-
-
-def get_sleep(oauth_client, start_date, results_file):
-    "Retrieve the sleep data for the day, store in datafile and return the dataframe."
-    # Retrieve the sleep data.  We need to translate the "value" if sleep into the different categories so
-    # it can be aligned with the heartbeat data.  Maping for the values are: 1-asleep, 2-restless, 3-awake
-    sleep = authd_client2.get_sleep(start_date)
-    logging.debug(json.dumps(sleep, indent=2))
-    t_list = []
-    v_list = []
-
-    # Only save the data if there is any.
-    if sleep['summary']['totalMinutesAsleep'] != 0:
-        for i in sleep['sleep'][0]['minuteData']:
-            v_list.append(i['value'])
-            t_list.append(i['dateTime'])
-    else:
-        logging.info("No sleep data for " + str(start_date))
-
-    df = pandas.DataFrame({'Time': t_list, start_date: v_list})
-    df.to_csv(results_file, columns=[
-              'Time', start_date], header=True, index=False)
-    return(df)
 
 def set_command_options():
     "Sets the command line arguments."
@@ -186,10 +76,11 @@ def set_command_options():
         prog='Fitbit Tracker', description=usage, formatter_class=argparse.RawDescriptionHelpFormatter)
     group = parser.add_mutually_exclusive_group(required=True)
     group_2 = parser.add_mutually_exclusive_group(required=True)
+
     parser.add_argument(
         'configfile', help='Name of the configuration file. (default: %(default)s)', type=str, default='config.json')
-    parser.add_argument('-d', '--debug_level',
-                        help='Set the debug level [debug info warn] (default: %(default)s)', action='store', type=str, default='info')
+    parser.add_argument('--log', '--log_level',  dest='log_level',
+                        help='Set the logging level [debug info warning error] (default: %(default)s)', action='store', type=str, default='info')
     parser.add_argument('-l', '--log_file', help='Set the logfile name. (default: %(default)s)',
                         action='store', type=str, default='fitbit-tracker.log')
     parser.add_argument('-o', '--output',  help='Output directory to store results files. (default: %(default)s)',
@@ -214,45 +105,71 @@ def set_command_options():
     args = parser.parse_args()
     return(parser)
 
+
 def get_command_options(parser):
     """ Retrieves the command line options and returns a kv dict """
+    global __DEBUG__
 
     args = parser.parse_args()
-    fmt = "%(asctime)-15s %(levelname)-8s %(message)s"
+    fmt = "%(asctime)-15s %(levelname)-8s %(lineno)5d:%(module)s:%(funcName)-25s %(message)s"
     log_file = args.log_file
     options = {'log_file': args.log_file}
-    logging.basicConfig(filename=args.log_file, format=fmt, level=logging.INFO)
+    line_sep = '--------------------------------------------------------------------------------'
+    msg = 'Starting ' + __TITLE__ + ' ' + __VERSION__
+
+    # Set up the logging infrastructure before we do anything.  To ensure we mark the start of a
+    # new instance, log the initialization using the level chosen.
+    # logging.basicConfig(filename=args.log_file, format=fmt, level=logging.INFO)
+    if args.log_level:
+        if 'debug' in args.log_level:
+            logging.basicConfig(filename=args.log_file,
+                                format=fmt, level=logging.DEBUG)
+            logging.debug(line_sep)
+            logging.debug(msg)
+            logging.debug(line_sep)
+            options['log_level'] = 'debug'
+            __DEBUG__ = True
+
+        elif 'warning' in args.log_level:
+            logging.basicConfig(filename=args.log_file,
+                                format=fmt, level=logging.WARNING)
+            logging.warning(line_sep)
+            logging.warning(msg)
+            logging.warning(line_sep)
+            options['log_level'] = 'warn'
+
+        elif 'error' in args.log_level:
+            logging.basicConfig(filename=args.log_file,
+                                format=fmt, level=logging.ERROR)
+            logging.error(line_sep)
+            logging.error(msg)
+            logging.error(line_sep)
+            options['log_level'] = 'error'
+
+        elif 'info' in args.log_level:
+            logging.basicConfig(filename=args.log_file,
+                                format=fmt, level=logging.INFO)
+            logging.info(line_sep)
+            logging.info(msg)
+            logging.info(line_sep)
+            options['log_level'] = 'info'
+
+        else:
+            logging.basicConfig(filename=args.log_file,
+                                format=fmt, level=logging.INFO)
+            logging.error('Invalid debug level.  Exiting the program.')
+            sys.exit(1)
 
     # Retrieve the data type(s) to collect.
     if args.all:
         options['collect_type'] = 'steps heartrate sleep'
     elif args.collect_type:
         options['collect_type'] = args.collect_type
-#        func = inspect.currentframe()
         logging.info('Collecting information for: ' + args.collect_type)
     else:
         logging.warning(
             'You need to specify the type of data to collect or use the -a flag')
         sys.exit(1)
-
-    # Set the desired logging level.  Default is error.
-    if args.debug_level:
-        if 'debug' in args.debug_level:
-            logging.basicConfig(level='DEBUG')
-            __DEBUG__ = True
-        elif 'warn' in args.debug_level:
-            logging.basicConfig(level=logging.WARNING)
-        elif 'info' in args.debug_level:
-            logging.basicConfig(level=logging.INFO)
-        elif 'error' in args.debug_level:
-            logging.basicConfig(level=logging.ERROR)
-        else:
-            logging.basicConfig(level=logging.ERROR)
-            logging.error('Invalid debug level: ' + args.debug_level + '. Exiting the program.')
-            sys.exit(1)
-    else:
-        logging.basicConfig(level=logging.INFO)
-    logging.info('Set the logging level to: ' + args.debug_level)
 
     # Use the specified configuration file.  There is no default.
     if args.configfile:
@@ -283,7 +200,8 @@ def get_command_options(parser):
         # Collect for a specific day
         if is_valid_date(args.date_to_collect):
             options['date_to_collect'] = args.date_to_collect
-            logging.info("Date to collect for: " + str(options['date_to_collect']))
+            logging.info("Date to collect for: " +
+                         str(options['date_to_collect']))
         else:
             print('Invalid date specified.  Exiting.')
             logging.error('Invalid date: ' + str(args.date_to_collect))
@@ -308,7 +226,6 @@ def get_command_options(parser):
             logging.info('End date: ' + args.end_date)
 
     else:
-        # Start and end date not specified.
         logging.error(
             'Both start and end dates need to be specified. Exiting.')
         sys.exit(1)
@@ -323,17 +240,136 @@ def get_command_options(parser):
     return(options)
 
 
+def refresh_new_token(token):
+    """Called when the access token needs to be refreshed. """
+    new_access_token = token['access_token']
+    new_refresh_token = token['refresh_token']
+    new_expires_at = token['expires_at']
+    logging.info('Refreshing token.')
+    logging.info('New token will expire at: ' + str(new_expires_at))
+
+    # open the configuration file and save the new tokens.
+    with open(CONFIG_FILE) as json_config_file:
+        data = json.load(json_config_file)
+
+    data['access_token'] = new_access_token
+    data['refresh_token'] = new_refresh_token
+    data['token_expires'] = new_expires_at
+    with open(CONFIG_FILE, 'w') as j_config_file:
+        json.dump(data, j_config_file, indent=4)
+
+
+def is_valid_date(date_to_check):
+    """ Checks to see if the date is valid """
+    year, month, day = date_to_check.split('-', 3)
+    try:
+        date(int(year), int(month), int(day))
+    except ValueError:
+        return(False)
+    return(True)
+
+
+def get_heartrate(oauth_client, start_date, time_interval, results_file):
+    """ Retrieve the intraday heartrate data and store to a file.
+
+    Args:
+      oauth_client:  An OAuth2 client id.
+      start_date:    Collect starting at this date
+      end_date:      Stope collecting at this date
+      time_interval: Time ganualarity to collect. See fitbit documentation
+      results_file:  The name of the file to store results in
+
+    Returns:
+      A pandas dataframe with the time and values.
+    """
+    hr = oauth_client.intraday_time_series(resource='activities/heart',
+                                           base_date=start_date,
+                                           detail_level=time_interval,
+                                           start_time='00:00',
+                                           end_time='23:59')
+    logging.debug(json.dumps(hr, indent=2))
+
+    # Only save the data if there is any.
+    if hr['activities-heart'][0]['value'] != 0:
+        t_list = []
+        v_list = []
+        for i in hr['activities-heart-intraday']['dataset']:
+            v_list.append(i['value'])
+            t_list.append(i['time'])
+    else:
+        logging.info("No heartrate data for " + str(start_date))
+
+    df = pandas.DataFrame({'Time': t_list, start_date: v_list})
+    df.to_csv(results_file, columns=[
+        'Time', start_date], header=True, index=False)
+    return(df)
+
+
+def get_steps(oauth_client, start_date, time_interval, results_file):
+    """Retrieve the step count for the day at the specified interval, store
+       data in a file and returns the data in a panda dataframe.
+    """
+    steps = oauth_client.intraday_time_series(resource='activities/steps',
+                                              base_date=start_date,
+                                              start_time='00:00',
+                                              end_time='23:59',
+                                              detail_level=time_interval)
+    logging.debug(json.dumps(steps, indent=2))
+
+    # Only save the data if there is any.
+    if steps['activities-steps'][0]['value'] != 0:
+        t_list = []
+        v_list = []
+        for i in steps['activities-steps-intraday']['dataset']:
+            v_list.append(i['value'])
+            t_list.append(i['time'])
+    else:
+        logging.info("No step data for " + str(start_date))
+
+    df = pandas.DataFrame({'Time': t_list, start_date: v_list})
+    df.to_csv(results_file, columns=[
+              'Time', start_date], header=True, index=False)
+    return(df)
+
+def get_sleep(oauth_client, start_date, results_file):
+    "Retrieve the sleep data for the day, store in datafile and return the dataframe."
+    # Retrieve the sleep data.  We need to translate the "value" if sleep into the different categories so
+    # it can be aligned with the heartbeat data.  Maping for the values are:
+    # 1=Asleep, 2=restless, 3=awake [NB: This is for fitbit api v1 only]
+    sleep = authd_client2.get_sleep(start_date)
+    logging.debug(json.dumps(sleep, indent=2))
+    t_list = list()
+    v_list = list()
+
+    # Only save the data if there is any.
+    if sleep['summary']['totalMinutesAsleep'] != 0:
+        # Account for days where there multiple sleep cycles.
+        num_cycles = len(sleep['sleep'])
+        x = 0
+        while (x<num_cycles):
+          for i in sleep['sleep'][x]['minuteData']:
+            v_list.append(i['value'])
+            t_list.append(i['dateTime'])
+          x = x + 1
+    else:
+        logging.info("No sleep data for " + str(start_date))
+    df = pandas.DataFrame({'Time': t_list, start_date: v_list})
+    df.to_csv(results_file, columns=[
+              'Time', start_date], header=True, index=False)
+    return(df)
+
 if __name__ == '__main__':
     parser = set_command_options()
     options = get_command_options(parser)
     CONFIG_FILE = options['config_file']
 
-    with open(CONFIG_FILE) as json_config_file:
+    with open(options['config_file']) as json_config_file:
         data = json.load(json_config_file)
 
     # Connect to the fitbit server using oauth2 See the page https://dev.fitbit.com/build/reference/web-api/oauth2/
     if data['access_token'] == '':
-        print("No access token found.  Please generate and place in the configuration file.")
+        print(
+            "No access token found.  Please generate and place in the configuration file.")
         logging.error('No access token found.  Exiting.')
         sys.exit(1)
 
@@ -349,8 +385,10 @@ if __name__ == '__main__':
                                       refresh_cb=refresh_new_token)
 
     except auth.exceptions.HTTPUnauthorized:
-        print('Please provide latest refresh and access tokens for oauth2. Exiting program.')
-        logging.error('Please provide latest refresh and access tokens for oauth2. Exiting program.')
+        print(
+            'Please provide latest refresh and access tokens for oauth2. Exiting program.')
+        logging.error(
+            'Please provide latest refresh and access tokens for oauth2. Exiting program.')
         sys.exit(1)
 
     # Note that that there is a limit of 150 api requests per hour. If the
@@ -373,11 +411,13 @@ if __name__ == '__main__':
         start_date = datetime.strptime(options['start_date'], '%Y-%m-%d')
         end_date = datetime.strptime(options['end_date'], '%Y-%m-%d')
         number_of_days_requested = end_date - start_date
-        number_of_days_requested_int = getattr(number_of_days_requested, 'days')
+        number_of_days_requested_int = getattr(
+            number_of_days_requested, 'days')
         logging.info('Startdate:' + str(start_date))
         logging.info('Enddate:' + str(end_date))
         logging.info('Days requested vale: ' + str(number_of_days_requested))
-        logging.info('Number of interger days requested: ' + str(number_of_days_requested_int))
+        logging.info('Number of interger days requested: ' +
+                     str(number_of_days_requested_int))
 
         if number_of_days_requested_int > max_days:
             logging.error(
@@ -418,7 +458,7 @@ if __name__ == '__main__':
                 steps_file = options['output_dir'] + '\\' + \
                     'steps_intraday_' + start_date_str + '.csv'
                 steps_df = get_steps(oauth_client=authd_client2, start_date=start_date_str,
-                                     time_interval='15min', results_file=steps_file)
+                                     time_interval='1min', results_file=steps_file)
 
             if 'sleep' in options['collect_type'] or 'all' in options['collect_type']:
                 sleep_file = options['output_dir'] + '\\' + \
